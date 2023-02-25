0a7e6e28af9c753e8a016150ebe6b965583b9746
jgabruk <jgabruk@gmail.com>
2023-02-25 18:36:32 +0100

    update1

--- project1/routes/web.php
+++ project1/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
+});
+
+Route::get('homepage', function () {
+    return view('homepage');
+   //return view("WSB");
 });
